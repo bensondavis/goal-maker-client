290e6a00bc7220d4d576d8348d709ecb2dfaf614
Benson Davis <bensondavis61@gmail.com>
2022-03-02 13:45:58 +0530

    bug fix

--- src/Convert.js
+++ src/Convert.js
@@ -9,7 +9,7 @@ function convert(result, safe) {
     month = result % 12;
     if (month !== 0) {
       return (
-        "Your goal can be achieved in " + year + " year and " + month + " month"
+        "Your goal can be achieved in " + year + " year and " + Math.ceil(month) + " month"
       );
     } else {
       return "Your goal can be achieved in " + year + " year";
